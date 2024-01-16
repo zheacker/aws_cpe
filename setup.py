3512c3347d69e5badc71541309a191983139b113
Zac Heacker <zheacker@users.noreply.github.com>
2024-01-16 10:07:32 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo for the AWS Cloud Practitioner Essentials course, the first course in the AWS Certified Cloud Practitioner exam.',
+    author='zac',
+    license='MIT',
+)
